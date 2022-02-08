7e33f582a450ef5a3414a182b3812e8398d5e480
Angelo <angeloabat25@gmail.com>
2022-02-07 18:22:24 -0800

    forge

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your Code Here
+let userName = window.prompt('Please enter your name:')
+let userScore= 0
+
+for(let x = 0; x < questions.length; x++){
+    let question = questions[x]
+    let Answer = window.prompt(question.text)
+    if (Answer === question.correctAnswer){
+        console.log("Correct!"), userScore = userScore + 10}
+    
+
+}
+
+window.alert("Your score is: " + userScore)
